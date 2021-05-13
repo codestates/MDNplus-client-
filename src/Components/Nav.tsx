43a47fe02f598460ea9fe224da9d851ccc87e31b
mooon3356 <mooon3356@gmail.com>
2021-05-14 01:11:46 +0900

    Update Nav.tsx(kakao login)

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -4,6 +4,8 @@ import LoginModal from "./LoginModal";
 import { useHistory } from "react-router";
 // axios.defaults.withCredentials = i;
 
+const { Kakao }: any = window;
+
 function Nav() {
   const [isLogin, setIsLogin] = useState(false);
   const [userName, setUserName] = useState("");
@@ -24,15 +26,14 @@ function Nav() {
   const kakaoAccessToken = (authorizationCode: string) => {
     console.log("카카오 accessToken 받는 요청 보내짐");
     console.log(authorizationCode);
-    axios.post("http://localhost:80/oauth/kakao", { authorizationCode: authorizationCode })
-    .then((res) => {
+    axios.post("http://localhost:80/oauth/kakao", { authorizationCode: authorizationCode }).then((res) => {
       console.log(res);
       const { accessToken, refreshToken } = res.data.data;
       if (accessToken) {
         setIsLogin(true);
-        localStorage.setItem("accessToken", accessToken);
+        Kakao.Auth.setAccessToken(accessToken);
         localStorage.setItem("refreshToken", refreshToken);
-        kakaoUserInfo()
+        kakaoUserInfo();
       }
     });
   };
@@ -50,32 +51,44 @@ function Nav() {
       });
   };
 
-  let header : any = `Bearer ${accessToken}`;
-
   const kakaoUserInfo = () => {
-    console.log('카카오 유저 정보 받아오는 요청 보내짐')
-    axios.get("https://kapi.kakao.com/v2/user/me", {headers: {Autorization: `Bearer ${accessToken}`}})
-    .then(res => console.log(res))
-
+    console.log("hi");
+    Kakao.API.request({
+      url: '/v2/user/me',
+      success: function(response:any) {
+        const {email} = response.kakao_account
+        const {nickname} = response.properties
+          console.log(email, nickname);
+      },
+      fail: function(error:any) {
+          console.log(error);
+      }
+  });
   };
-  // const kakaoUserInfo = async () => {
-  //   console.log("카카오 유저 정보 받아오는 요청 보내짐");
-  //   // await axios
-  //   //   .get("https://kapi.kakao.com/v2/user/me", { headers: { Authorization: `Bearer ${accessToken}` } })
-  //   //   .then((res) => console.log("요청 성공"))
-  //   //   .catch((err) => console.log(err));
-  //   // Kakao.API.request({
-  //   //   url: '/v2/user/me',
-  //   //   success: function(response) {
-  //   //       console.log(response);
-  //   //   },
-  //   //   fail: function(error) {
-  //   //       console.log(error);
-  //   //   }
-  // };
+
+  const handleLogout = () => {
+    if (!Kakao.Auth.getAccessToken()) {
+      console.log('Not logged in.');
+      return;
+    }
+    Kakao.Auth.logout(function() {
+      console.log(Kakao.Auth.getAccessToken());
+      setIsLogin(false)
+    });
+    // Kakao.API.request({
+    //   url: '/v1/user/unlink',
+    //   success: function(response:any) {
+    //     console.log(response);
+    //   },
+    //   fail: function(error:any) {
+    //     console.log(error);
+    //   },
+    // });
+  }
 
   // loginModal에서 깃허브 로그인 성공 후, 리디렉션이 되어서 localhost로 돌아오면 실행되는 라이프사이클 코드
   useEffect(() => {
+    // console.log(Kakao.isInitialized())
     const url = new URL(window.location.href); // 현재 위치하는 웹사이트의 url을 가져옴
     const authorizationCode = url.searchParams.get("code"); // 깃허브로부터 받은 인증코드를 가져옴 ex) http://localhost:3000/?code=5e52fb85d6a1ed46a51f 여기서 code 뒤의 숫자들이 인증코드
     if (url.pathname === "/kakaoLogin") {
@@ -95,8 +108,10 @@ function Nav() {
   return (
     <div>
       <LoginModal />
+      <div></div>
       {isLogin ? <div>로그인 되어 있음</div> : <div>로그인 안되어 있음</div>}
       {userName ? <div>{userName}</div> : <div>아직 깃허브에서 유저정보 안가져옴</div>}
+      <button onClick={handleLogout}>로그아웃</button>
     </div>
   );
 }

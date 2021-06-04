09dff15641b4fe5087a881b062bf6df3c345132c
Junrae Kim <jrk9204@gmail.com>
2021-06-04 13:25:43 +0900

    Fixed footer css

--- src/Components/FooterComponent.tsx
+++ src/Components/FooterComponent.tsx
@@ -4,44 +4,115 @@ function FooterComponent() {
   return (
     <Container>
       <ContentContainer>
-        <TopContent>BO3 Company &nbsp; </TopContent>
-        <BottomContent>&nbsp;Front-End : 문성석 김준래 &nbsp; &nbsp;Back-End : 조은길 김승용</BottomContent>
+        <LogoContainer>
+          <Logo>MDN+ </Logo>
+          <CopyRight>ⓒ 2021. MDN+ all rights reserved.</CopyRight>
+        </LogoContainer>
+
+        <Company>
+          <Title>Company</Title>
+          <Contents>
+            <div>About us</div>
+            <div>Jobs</div>
+            <div>Contact</div>
+          </Contents>
+        </Company>
+        <Product>
+          <Title>Product</Title>
+          <Contents>
+            <div>Features</div>
+            <div>Pricing</div>
+            <div>Apps</div>
+            <div>Support</div>
+          </Contents>
+        </Product>
+        <Resources>
+          <Title>Resources</Title>
+          <Contents>
+            <div>Education</div>
+            <div>Wrok</div>
+          </Contents>
+        </Resources>
+        <AboutUs>
+          <Title> About us </Title>
+
+          <Contents>
+            <div>
+              <Atag href="https://github.com/codestates/MDNplus-client-">Visit US!</Atag>
+            </div>
+            <div>Seongseok Moon</div>
+            <div>Junrae Kim</div>
+            <div>Eungil Cho</div>
+            <div>Seungyong Kim</div>
+          </Contents>
+        </AboutUs>
       </ContentContainer>
     </Container>
   );
 }
 
+//&nbsp;Front-End : 문성석 김준래 &nbsp; &nbsp;Back-End : 조은길 김승용
+
 export default FooterComponent;
 
 const Container = styled.div`
-  background: #1a237e;
+  background: #f5f5f5;
   width: 100%;
-  height: 10rem;
+  height: 15rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   @media (max-width: 375px) {
     display: none;
   }
 `;
 
 const ContentContainer = styled.div`
+  color: #616161;
   display: flex;
-
-  align-items: center;
-  font-size: 1.3rem;
-
-  color: #e0e0e0;
-  width: 70%;
+  justify-content: space-between;
+  width: 65%;
   height: 100%;
+  font-size: 1.2rem;
+  padding: 3rem 0 3rem 0;
 `;
 
-const TopContent = styled.span`
-  padding-bottom: 0.5rem;
-  font-size: 2rem;
+const LogoContainer = styled.span`
+  font-size: 2.2rem;
+  font-weight: bold;
 `;
 
-const BottomContent = styled.span`
-  line-height: 2rem;
-  font-size: 1rem;
+const Logo = styled.div`
+  font-family: sans-serif;
+`;
+
+const Company = styled.div``;
+
+const Product = styled.div``;
+
+const Resources = styled.span``;
+
+const AboutUs = styled.span``;
+
+const Title = styled.span`
+  font-weight: bold;
+  font-size: 1.3rem;
+`;
+
+const Contents = styled.span`
+  font-size: 0.8rem;
+  line-height: 1.8rem;
+  font-weight: normal;
+  cursor: pointer;
+`;
+
+const CopyRight = styled.div`
+  font-size: 0.8rem;
+  margin-top: 1rem;
+`;
+
+const Atag = styled.a`
+  color: #616161;
+  text-decoration: none;
 `;

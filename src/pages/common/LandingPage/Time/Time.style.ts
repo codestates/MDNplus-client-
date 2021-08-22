ef20b4a1be420c8b1f04ff18c6ba03ee7aaba49b
mooon3356 <mooon3356@gmail.com>
2021-08-22 21:39:53 +0900

    Refactor: Landing/Time css

--- src/pages/common/LandingPage/Time/Time.style.ts
+++ src/pages/common/LandingPage/Time/Time.style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { fadeIn } from "../../../../styled-components/Animation";
 
-export const TimeContainer_ON = styled.div`
+export const Container_ON = styled.div`
   opacity: 1;
   width: 100%;
   height: 31rem;
@@ -14,34 +14,38 @@ export const TimeContainer_ON = styled.div`
   animation-timing-function: ease-out;
   animation-name: ${fadeIn};
   animation-fill-mode: forwards;
+
+  .time-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .time-img {
+    width: 2rem;
+    height: 2rem;
+    margin-bottom: 1rem;
+  }
+
+  .time-title {
+    font-size: 2rem;
+    color: #757575;
+    margin-bottom: 1rem;
+  }
+
+  .time {
+    font-size: 3rem;
+    font-weight: bold;
+  }
+
+  .time-sub {
+    margin-top: 0.3rem;
+  }
 `;
-export const TimeContainer_OFF = styled.div`
+
+export const Container_OFF = styled.div`
   opacity: 0;
   width: 100%;
   height: 30rem;
-`;
-export const TimeBox = styled.div`
-  // border: 1px solid black;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-export const TimeImg = styled.img`
-  width: 2rem;
-  height: 2rem;
-  margin-bottom: 1rem;
-`;
-export const TimeBoxTitle = styled.div`
-  font-size: 2rem;
-  color: #757575;
-  margin-bottom: 1rem;
-`;
-
-export const Time = styled.div`
-  font-size: 3rem;
-  font-weight: bold;
-`;
-export const TimeSub = styled.div`
-  margin-top: 0.3rem;
 `;
--- src/pages/common/LandingPage/Time/index.tsx
+++ src/pages/common/LandingPage/Time/index.tsx
@@ -1,12 +1,4 @@
-import {
-  Time,
-  TimeBox,
-  TimeBoxTitle,
-  TimeContainer_OFF,
-  TimeContainer_ON,
-  TimeImg,
-  TimeSub,
-} from "./Time.style";
+import { Container_OFF, Container_ON } from "./Time.style";
 
 type NavContainerProps = {
   currentY: number;
@@ -16,16 +8,19 @@ function TimeSection({ currentY }: NavContainerProps) {
   return (
     <>
       {currentY >= 260 ? (
-        <TimeContainer_ON>
-          <TimeBox>
-            <TimeImg src="https://res.cloudinary.com/dr4ka7tze/image/upload/v1629112349/clockIcon_qedx9c.png"></TimeImg>
-            <TimeBoxTitle>MDN+로 아낀 개발 검색 시간</TimeBoxTitle>
-            <Time>153,352분</Time>
-            <TimeSub>* 2021년 6월 설문조사 기준</TimeSub>
-          </TimeBox>
-        </TimeContainer_ON>
+        <Container_ON>
+          <div className="time-box">
+            <img
+              className="time-img"
+              src="https://res.cloudinary.com/dr4ka7tze/image/upload/v1629112349/clockIcon_qedx9c.png"
+            ></img>
+            <div className="time-title">MDN+로 아낀 개발 검색 시간</div>
+            <div className="time">153,352분</div>
+            <div className="time-sub">* 2021년 6월 설문조사 기준</div>
+          </div>
+        </Container_ON>
       ) : (
-        <TimeContainer_OFF></TimeContainer_OFF>
+        <Container_OFF></Container_OFF>
       )}
     </>
   );
